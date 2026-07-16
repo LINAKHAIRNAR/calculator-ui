5b304bcce115264ba5f340fcf05642e0696473c0
Lina Khairnar <linakhairnar25@gmail.com>
2026-07-16 16:59:35 +0530

    file modified colon button added

--- script.js
+++ script.js
@@ -17,7 +17,46 @@
         input.value=input.value.slice(0,-1)
         //input.value=input.value.substring(0,input.value.length-1)
     }
+    function colon(){
+        let n;
+        let arr=input.value.split("")
 
+        let open=arr.filter(ch=>ch==="("
+        ).length;
+
+        let close=arr.filter(ch=>ch===")"
+        ).length;
+
+        // console.log(open)
+        // console.log(close)
+
+        if(open===close)
+        {
+        arr.push("(")
+        n= arr.join("")
+        input.value=n
+        }
+        else{
+         arr.push(")")
+         n= arr.join("")
+         input.value=n
+
+        }
+
+        // if(!arr.includes("("))
+        // {
+        // arr.push("(")
+        // n= arr.join("")
+        // input.value=n
+        // }
+        // else{
+        // arr.push(")")
+        // n= arr.join("")
+        // input.value=n
+        // }
+        
+        
+    }
 
     
 
